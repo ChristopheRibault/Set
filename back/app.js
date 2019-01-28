0437c32ae52b82e8a5f52b6ba548028e641082ea
ChristopheRibault <c.ribault.dev@gmail.com>
2019-01-28 10:45:01 +0100

    first route

--- back/app.js
+++ back/app.js
@@ -10,6 +10,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
-
+app.route('/')
+  .get(async (req, res) => {
+    const result = await knex.select().from('cards');
+    res.status(200).send(result);
+  })
 
 app.listen(port, console.log(`server listening on port ${port}`));
