2c9fae6db3f3db03e37d2674599942aa5d3e63ec
ChristopheRibault <c.ribault.dev@gmail.com>
2019-01-28 16:11:44 +0100

    refacto checkGame

--- back/helpers/checkGame.js
+++ back/helpers/checkGame.js
@@ -4,14 +4,10 @@ module.exports = (cards) => {
   const sets = [];
   for (let i = 0; i < cards.length - 2; i++){
     for (let j = i + 1; j < cards.length - 1; j++){
-      if (j !== i){
-        for (let k = j + 1; k < cards.length; k++){
-          if(k !== j){
-            let hand = [cards[i], cards[j], cards[k]]
-            if (checkSet(hand))
-            sets.push(hand);
-          }
-        }
+      for (let k = j + 1; k < cards.length; k++){
+        let hand = [cards[i], cards[j], cards[k]]
+        if (checkSet(hand))
+        sets.push(hand);
       }
     }
   }
