4a338cad5e1e0c74030c55a251ac55d8dc608872
ChristopheRibault <c.ribault.dev@gmail.com>
2019-01-28 16:08:02 +0100

    correction checkGame function

--- back/helpers/checkGame.js
+++ back/helpers/checkGame.js
@@ -3,9 +3,9 @@ const checkSet = require('./checkSet');
 module.exports = (cards) => {
   const sets = [];
   for (let i = 0; i < cards.length - 2; i++){
-    for (let j = 1; j < cards.length - 1; j++){
+    for (let j = i + 1; j < cards.length - 1; j++){
       if (j !== i){
-        for (let k = 2; k < cards.length; k++){
+        for (let k = j + 1; k < cards.length; k++){
           if(k !== j){
             let hand = [cards[i], cards[j], cards[k]]
             if (checkSet(hand))
