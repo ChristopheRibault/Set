a14497423025a171ac42a021ded5dfa2a9a627c6
Widaad Barreto <widaad.barreto@gmail.com>
2019-01-28 22:35:03 +0100

    getting the player names in the game board

--- views/src/components/Game/GameTools.js
+++ views/src/components/Game/GameTools.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class GameTools extends Component {
   render() {
+    const { numberOfPlayers, playerNames } = this.props;
+    console.log("gametools", this.props.numberOfPlayers, this.props.playerNames)
     return (
+      playerNames.map((player)=>{
+        return(
       <div>
-        
+        {player}
       </div>
+      )})
     )
   }
 }
