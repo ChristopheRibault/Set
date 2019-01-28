12927252c4d12b730a79204092849edaad4a81b5
Widaad Barreto <widaad.barreto@gmail.com>
2019-01-28 23:29:05 +0100

    button set disabled when it is time to play

--- views/src/components/Game/GameTools.js
+++ views/src/components/Game/GameTools.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import "./GameTools.css";
 
 class GameTools extends Component {
+  state = {
+    playerNamePlaying: '',
+    playingTime: false,
+  }
+
+  getNamePlayerPlaying = async (e) => {
+    await this.setState({playerNamePlaying: e.target.name, playingTime: true})
+    setTimeout(()=>this.setState({playerNamePlaying: '', playingTime: false}), 5000)
+  }
+
   render() {
-    const { numberOfPlayers, playerNames } = this.props;
+    const { playerNames } = this.props;
+    const { playingTime } = this.state;
     console.log("gametools", this.props.numberOfPlayers, this.props.playerNames)
+    console.log("playerNamePlaying", this.state.playerNamePlaying)
     return (
       playerNames.map((player)=>{
         return(
       <div>
-        {player}
+        {player} <button onClick={this.getNamePlayerPlaying} className={playingTime ? "disabled" : "active"} name={player}>SET !</button>
       </div>
       )})
     )
