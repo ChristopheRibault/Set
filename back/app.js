4bedb5aa6d07618405e24468ce48f3817f8ab8c9
ChristopheRibault <c.ribault.dev@gmail.com>
2019-01-28 10:42:54 +0100

    filling DB

--- back/app.js
+++ back/app.js
@@ -11,4 +11,5 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 
 
+
 app.listen(port, console.log(`server listening on port ${port}`));
---
+++ back/db/migrations/20190128095602_cards.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('cards', (table) => {
+    table.increments();
+    table.string('code').notNullable().unique();
+    table.string('shape').notNullable();
+    table.string('color').notNullable();
+    table.string('filling').notNullable();
+    table.string('quantity').notNullable();
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('cards');
+};
