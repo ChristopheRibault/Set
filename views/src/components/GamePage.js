ce6c47866ea8505dc6a4cf9a2ac4ffc411b8271e
Widaad Barreto <widaad.barreto@gmail.com>
2019-01-28 16:29:23 +0100

    refactoring

--- views/src/components/GamePage.js
+++ views/src/components/GamePage.js
@@ -14,34 +14,40 @@ class GamePage extends Component {
     const { allCards } = this.state;
     const twelve = allCards.splice(0, 12);
     this.setState({ gameCards: twelve });
-  }
+	}
+
+	removeThreeCards = () => {
+		for (let i = 0; i < 3; i++) {
+			const selectCardsCode = (object) => {
+				return object.code === this.state.selectedCards[i].code;
+			};
+			const gameCardIndex = this.state.gameCards.findIndex(selectCardsCode)
+			this.state.gameCards.splice(gameCardIndex, 1);
+		}
+	}
+
+	giveMeThreeCards = () => {
+		if(this.state.gameCards.length < 12){
+			const three = this.state.allCards.splice(0,3)
+			this.state.gameCards.push(three[0], three[1], three[2])
+			this.setState({gameCards: this.state.gameCards})
+		}
+	}
 
   recordValue = async card => {
-    const { selectedCards } = this.state;
-    this.state.selectedCards.push(card);
+		const { selectedCards } = this.state;
 
-    await this.setState({ selectedCards });
+    this.state.selectedCards.push(card);
+		await this.setState({ selectedCards });
 
     if (this.state.selectedCards.length === 3) {
       const res = await axios.post("http://localhost:5000/checkSet", {
         cards: this.state.selectedCards
-      });
-      if (!res.data) {
-				const { gameCards, allCards } = this.state;
+			});
 
-        for (let i = 0; i < 3; i++) {
-          const selectedCardsCode = object => {
-            return object.code === selectedCards[i].code;
-          };
-          const gameCardIndex = gameCards.findIndex(selectedCardsCode)
-          gameCards.splice(gameCardIndex, 1);
-				}
-				if(this.state.gameCards.length < 12){
-					const three = allCards.splice(0,3)
-					console.log('three', three)
-					this.state.gameCards.push(three[0], three[1], three[2])
-					this.setState({gameCards})
-				}
+      if (res.data) {
+        this.removeThreeCards()
+				this.giveMeThreeCards()
 			}
 		this.setState({ selectedCards: [] });
 		};
