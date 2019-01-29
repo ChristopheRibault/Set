97e5596e64103edf29851c1134f2cecb5e09f5dd
Elisa <elucchini17@gmail.com>
2019-01-29 12:23:06 +0100

    small modifications

--- views/src/components/Game/GameBoard/Card.js
+++ views/src/components/Game/GameBoard/Card.js
@@ -5,14 +5,6 @@ const domain = process.env.REACT_APP_DOMAIN_NAME;
 
 
 export default class Card extends Component {
-  displayCard = () => {
-    const { card } = this.props;
-    const array = [];
-    for (let i = 0; i < card.quantity; i++){
-      array[i] = 1;
-    }
-    array.map(e => console.log(e));
-  }
 
   render() {
     const { card } = this.props;
@@ -21,8 +13,6 @@ export default class Card extends Component {
       array[i] = 1;
     }
 
-    this.displayCard();
-    
     return (
       <div className= 'Card'>
         {array.map((e, i) => <img key={i} src={`${domain}${card.image}`} alt={card.code}/>)}
